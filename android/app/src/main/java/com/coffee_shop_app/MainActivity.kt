4651df476c64b4e6ae257651d05b50f68fa40f1b
Victor <vector6971@gmail.com>
2024-04-26 17:57:08 -0400

    Enviroment

--- android/app/src/main/java/com/coffee_shop_app/MainActivity.kt
+++ android/app/src/main/java/com/coffee_shop_app/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.coffee_shop_app
 
+import android.os.Bundle;
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
@@ -12,6 +13,12 @@ class MainActivity : ReactActivity() {
    * rendering of the component.
    */
   override fun getMainComponentName(): String = "Coffee_Shop_App"
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    // SplashScreen.show(this)
+    super.onCreate(null)
+}
+
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
