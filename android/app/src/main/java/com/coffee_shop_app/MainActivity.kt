2b5a7b5d78c5f59ab19ee32229148be6cbe0189e
Victor <vector6971@gmail.com>
2024-05-01 16:06:28 -0400

    Last Changes!

--- android/app/src/main/java/com/coffee_shop_app/MainActivity.kt
+++ android/app/src/main/java/com/coffee_shop_app/MainActivity.kt
@@ -1,29 +1,44 @@
 package com.coffee_shop_app
 
-import android.os.Bundle;
+import android.os.Bundle
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
-import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
+import org.devio.rn.splashscreen.SplashScreen
 
-class MainActivity : ReactActivity() {
-
-  /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
-   * rendering of the component.
-   */
-  override fun getMainComponentName(): String = "Coffee_Shop_App"
-
-  override fun onCreate(savedInstanceState: Bundle?) {
-    // SplashScreen.show(this)
-    super.onCreate(null)
+// Aquí agregamos la definición del objeto DefaultNewArchitectureEntryPoint
+object DefaultNewArchitectureEntryPoint {
+    fun getFabricEnabled(): Boolean {
+        // Aquí debes retornar el valor que determina si Fabric está habilitado o no.
+        // Por ahora, retornaremos false como un valor predeterminado.
+        return false
+    }
 }
 
+class MainActivity : ReactActivity() {
+    /**
+     * Returns the name of the main component registered from JavaScript. This is used to schedule
+     * rendering of the component.
+     */
+    override fun getMainComponentName(): String {
+        return "Coffee_Shop_App"
+    }
 
-  /**
-   * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
-   * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
-   */
-  override fun createReactActivityDelegate(): ReactActivityDelegate =
-      DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        SplashScreen.show(this)
+        super.onCreate(savedInstanceState)
+    }
+
+    /**
+     * Returns the instance of the [ReactActivityDelegate]. Here we use a util class [
+     * DefaultReactActivityDelegate] which allows you to easily enable Fabric and Concurrent React
+     * (aka React 18) with two boolean flags.
+     */
+    override fun createReactActivityDelegate(): ReactActivityDelegate {
+        return DefaultReactActivityDelegate(
+            this,
+            mainComponentName,
+            DefaultNewArchitectureEntryPoint.getFabricEnabled()
+        )
+    }
 }
